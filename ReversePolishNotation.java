2ad78e25b875b146ebcaba73e4a819c2d0c294d2
me <thebeliever3@gmail.com>
2020-07-17 22:45:26 +0300

    Now working with multi-digit numbers

--- ReversePolishNotation.java
+++ ReversePolishNotation.java
@@ -1,92 +1,93 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Stack;
 
 public class ReversePolishNotation {
-    private StringBuilder convertToRPN(String inputString) {
-        StringBuilder convertedString = new StringBuilder();
-        Stack<Character> stack = new Stack<>();
-        if(inputString.charAt(0) == '-') {
-            convertedString.append('0');
-            stack.push('-');
+    private static List<String> convertToRPN(String inputString) {
+        Stack<String> stack = new Stack<>();
+        List<String> convertedString = new ArrayList<>();
+        String[] tokenArr = inputString.split("\\s+|((?<=[()*/+-])|(?=[()*/+-]))");
+        List<String> tokenList = new ArrayList<>(Arrays.asList(tokenArr));
+
+        if (tokenList.get(0).equals("-")) {
+            tokenList.set(0, "-1");
+            tokenList.add(1, "*");
         }
-        else {
-            for (int i = 0, n = inputString.length(); i < n; i++) {
-                char token = inputString.charAt(i);
-                if (Character.isDigit(token)) {
-                    convertedString.append(token);
-                } else {
-                    switch (token) {
-                        case '(':
-                            stack.push('(');
-                            if(inputString.charAt(i+1) == '-') {
-                                convertedString.append('0');
-                                stack.push('-');
-                                i++;
-                            }
+
+        for (int i = 0; i < tokenList.size(); i++) {
+                if (tokenList.get(i).matches("-?\\d+")) {
+                    convertedString.add(tokenList.get(i));
+                    continue;
+
+                } else if (tokenList.get(i).matches("[(*/+-]") && tokenList.get(i + 1).equals("-")) {
+                    tokenList.set(i + 1, "-1");
+                    tokenList.add(i + 2, "*");
+                }
+                switch (tokenList.get(i)) {
+                        case "(":
+                            stack.push("(");
                             break;
 
-                        case ')':
-                            while(stack.peek() != '(')
-                                convertedString.append(stack.pop());
+                        case ")":
+                            while (!stack.peek().equals("("))
+                                convertedString.add(stack.pop());
                             stack.pop();
                             break;
 
-                        case '*':
-                        case '/':
-                            if(!stack.empty() && (stack.peek() == '*' || stack.peek() == '/'))
-                                convertedString.append(stack.pop());
-                            stack.push(token);
+                        case "*":
+                        case "/":
+                            while (!stack.empty() && (stack.peek().equals("*") || stack.peek().equals("/")))
+                                convertedString.add(stack.pop());
+                            stack.push(tokenList.get(i));
                             break;
 
-                        case '+':
-                        case '-':
-                            if(!stack.empty() && (stack.peek() != '('))
-                                convertedString.append(stack.pop());
-                            stack.push(token);
+                        case "+":
+                        case "-":
+                            while (!stack.empty() && !stack.peek().equals("("))
+                                convertedString.add(stack.pop());
+                            stack.push(tokenList.get(i));
                             break;
 
                         default:
                             break;
-                    }
                 }
-            }
         }
-        while(!stack.empty())
-            convertedString.append(stack.pop());
+
+        while (!stack.empty())
+            convertedString.add(stack.pop());
         return convertedString;
     }
     int calculate(String mathString) {
-        StringBuilder converted = convertToRPN(mathString);
+        List<String> converted = convertToRPN(mathString);
         Stack<Integer> numStack = new Stack<>();
         int secondOperand;
-        for(int i = 0, n = converted.length(); i < n; i++){
-            char token = converted.charAt(i);
-            switch (token){
-                case '*':
+        for (int i = 0, n = converted.size(); i < n; i++) {
+            switch (converted.get(i)) {
+                case "*":
                     numStack.push(numStack.pop() * numStack.pop());
                     break;
-                case '/':
+                case "/":
                     secondOperand = numStack.pop();
                     numStack.push(numStack.pop() / secondOperand);
                     break;
-                case '+':
+                case "+":
                     numStack.push(numStack.pop() + numStack.pop());
                     break;
-                case '-':
+                case "-":
                     secondOperand = numStack.pop();
                     numStack.push(numStack.pop() - secondOperand);
                     break;
                 default:
-                    numStack.push(token - '0');
-                    System.out.println(numStack);
+                    numStack.push(Integer.valueOf(converted.get(i)));
                     break;
             }
         }
         return numStack.pop();
     }
-    public static void main(String[] args){
+    public static void main(String[] args) {
         ReversePolishNotation example = new ReversePolishNotation();
-        System.out.println(example.convertToRPN("2*(3+2*(1+2*(1+3)))"));
-        System.out.println(example.calculate("2*(3+2*(1+2*(1+3)))"));
+        int result = example.calculate("-(72/12-8*-(-4)+7)");
+        System.out.println(result);
     }
 }
