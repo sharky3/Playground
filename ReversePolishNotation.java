5e6ccb7ddd72bd98ff4a8a7d659338cbd0f1fa1e
me <thebeliever3@gmail.com>
2020-07-18 21:55:39 +0300

    Improved string parsing and added RPN-string evaluation support.

--- ReversePolishNotation.java
+++ ReversePolishNotation.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Stack;
 
 public class ReversePolishNotation {
-    private static List<String> convertToRPN(String inputString) {
+
+    static List<String> convertToRPN(String inputString) {
         Stack<String> stack = new Stack<>();
         List<String> convertedString = new ArrayList<>();
-        String[] tokenArr = inputString.split("\\s+|((?<=[()*/+-])|(?=[()*/+-]))");
+
+        inputString = inputString.replaceAll("\\s+", "");
+        String[] tokenArr = inputString.split("(?<=[()*/+-])|(?=[()*/+-])");
         List<String> tokenList = new ArrayList<>(Arrays.asList(tokenArr));
 
         if (tokenList.get(0).equals("-")) {
@@ -16,68 +19,80 @@ public class ReversePolishNotation {
         }
 
         for (int i = 0; i < tokenList.size(); i++) {
-                if (tokenList.get(i).matches("-?\\d+")) {
-                    convertedString.add(tokenList.get(i));
-                    continue;
+            if (tokenList.get(i).matches("-?\\d+")) {
+                convertedString.add(tokenList.get(i));
+                continue;
 
-                } else if (tokenList.get(i).matches("[(*/+-]") && tokenList.get(i + 1).equals("-")) {
-                    tokenList.set(i + 1, "-1");
-                    tokenList.add(i + 2, "*");
-                }
-                switch (tokenList.get(i)) {
-                        case "(":
-                            stack.push("(");
-                            break;
+            } else if (tokenList.get(i).matches("[(*/+-]") && tokenList.get(i + 1).equals("-")) {
+                tokenList.set(i + 1, "-1");
+                tokenList.add(i + 2, "*");
+            }
+            switch (tokenList.get(i)) {
+                case "(":
+                    stack.push("(");
+                    break;
 
-                        case ")":
-                            while (!stack.peek().equals("("))
-                                convertedString.add(stack.pop());
-                            stack.pop();
-                            break;
+                case ")":
+                    while (!stack.peek().equals("("))
+                        convertedString.add(stack.pop());
+                    stack.pop();
+                    break;
 
-                        case "*":
-                        case "/":
-                            while (!stack.empty() && (stack.peek().equals("*") || stack.peek().equals("/")))
-                                convertedString.add(stack.pop());
-                            stack.push(tokenList.get(i));
-                            break;
+                case "*":
+                case "/":
+                    while (!stack.empty() && (stack.peek().equals("*") || stack.peek().equals("/")))
+                        convertedString.add(stack.pop());
+                    stack.push(tokenList.get(i));
+                    break;
 
-                        case "+":
-                        case "-":
-                            while (!stack.empty() && !stack.peek().equals("("))
-                                convertedString.add(stack.pop());
-                            stack.push(tokenList.get(i));
-                            break;
+                case "+":
+                case "-":
+                    while (!stack.empty() && !stack.peek().equals("("))
+                        convertedString.add(stack.pop());
+                    stack.push(tokenList.get(i));
+                    break;
 
-                        default:
-                            break;
-                }
+                default:
+                    break;
+            }
         }
 
         while (!stack.empty())
             convertedString.add(stack.pop());
         return convertedString;
     }
-    int calculate(String mathString) {
-        List<String> converted = convertToRPN(mathString);
+
+    static int calculate(String mathString, boolean ready) {
+        List<String> converted;
+
+        if (ready)
+            converted = new ArrayList<String>(Arrays.asList(mathString.split("\\s+|,\\s*")));
+        else
+            converted = convertToRPN(mathString);
+
         Stack<Integer> numStack = new Stack<>();
         int secondOperand;
+
         for (int i = 0, n = converted.size(); i < n; i++) {
             switch (converted.get(i)) {
                 case "*":
                     numStack.push(numStack.pop() * numStack.pop());
                     break;
+
                 case "/":
                     secondOperand = numStack.pop();
                     numStack.push(numStack.pop() / secondOperand);
                     break;
+
                 case "+":
                     numStack.push(numStack.pop() + numStack.pop());
                     break;
+
                 case "-":
                     secondOperand = numStack.pop();
                     numStack.push(numStack.pop() - secondOperand);
                     break;
+
                 default:
                     numStack.push(Integer.valueOf(converted.get(i)));
                     break;
@@ -85,9 +100,11 @@ public class ReversePolishNotation {
         }
         return numStack.pop();
     }
+
     public static void main(String[] args) {
-        ReversePolishNotation example = new ReversePolishNotation();
-        int result = example.calculate("-(72/12-8*-(-4)+7)");
+        int resultReady = calculate("-1 72 12 / 8 -1 * -1 4 * * - 7 + *", true);
+        System.out.println(resultReady);
+        int result = calculate("-(72 / 12 - 8 * -(-4) + 7)", false);
         System.out.println(result);
     }
 }
